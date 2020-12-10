c07dfad3a179784db0a2a4c5d61972914002f46b
Technique-VPSI <technique.vpsi@gmail.com>
2020-12-10 13:20:11 +0100

    add manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,29 +1,11 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "ctr",
-
-    'summary': """
-        """,
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "lamrabti abdellatif",
-    'website': "http://www.vpsi.odoo.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['helpdesk'],
-
-    # always loaded
-    'data': [
-        'security/ir.model.access.csv',
-        'views/views.xml',
+    'name':'ctr',
+    'summary':'adding some fields',
+    'version':'0.1',
+    "depends":[
+        "helpdesk",
     ],
+    "data":[
+        'views/views.xml',
+    ]
 }

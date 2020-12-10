92aede9e66fa40d1b2e9f5b77b1b9eacf4995c06
Technique-VPSI <technique.vpsi@gmail.com>
2020-12-10 16:45:53 +0100

    deleting marque and product

--- models/models.py
+++ models/models.py
@@ -6,5 +6,4 @@ from odoo import models, fields
 class ctr(models.Model):
     _inherit = 'helpdesk.ticket'
     numero_ticket = fields.Char(string="Numéro de ticket")
-    produit = fields.selection([(),(),(),()])
-    marque = fields.selection([(),(),(),(),()])
+

fb165bcfe876d58d2c787ee646c64b37d1b69b72
Technique-VPSI <technique.vpsi@gmail.com>
2020-12-10 12:48:47 +0100

    a

--- models/models.py
+++ models/models.py
@@ -3,13 +3,7 @@
 from odoo import models, fields, api
 
 
- class ctr(models.Model):
-     _name = 'ctr.ctr'
-     _inherit="helpdesk.ticket"
-
-     numero_ticket = fields.Char(string="Numéro de ticket")
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+class ctr(models.Model):
+    _name = 'ctr.ctr'
+    _inherit="helpdesk.ticket"
+    numero_ticket = fields.Char(string="Numéro de ticket")

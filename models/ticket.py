74957f980c5b9646685018d5f3ae8176d528923e
Technique-VPSI <technique.vpsi@gmail.com>
2020-12-24 09:59:06 +0100

    adding fields

--- models/ticket.py
+++ models/ticket.py
@@ -6,4 +6,8 @@ from odoo import models, fields
 class ctr(models.Model):
     _inherit = 'helpdesk.ticket'
     numero_ticket = fields.Char(string="Numéro de ticket")
+    reference=fields.Char(string="Réference")
+    objet_reclamation=fields.Char(string="Objet de la reclamation")
+    date_reclamation=fields.Date(string="Date de raclamation")
+    date_proch_action=fields.Date(string="Date de prochain action")
 

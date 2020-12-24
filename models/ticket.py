989b85834ef77c9f47db0fafab60cdf6678ae1c8
Technique-VPSI <technique.vpsi@gmail.com>
2020-12-24 10:13:55 +0100

    add product and mark classes

--- models/ticket.py
+++ models/ticket.py
@@ -11,3 +11,12 @@ class ctr(models.Model):
     date_reclamation=fields.Date(string="Date de raclamation")
     date_proch_action=fields.Date(string="Date de prochain action")
 
+class product(models.Model):
+    _inherit= 'helpdesk.ticket'
+    code_produit=fields.Char(string='Code')
+    libelle_produit=fields.Char(string='Produit')
+
+class marque(models.Model):
+    _inherit= 'helpdesk.ticket'
+    code_marque=fields.Char(string='Code')
+    libelle_marque=fields.Char(string='Marque')
